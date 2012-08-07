9744e8239468da87604e2ecae00003bd486d7789
RegulusNero <regulus.nero@gmail.com>
2012-08-07 11:49:05 +0200

    leider nicht viel geschafft

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include <ctype.h>
 
 void kartenErstellen(void);
+void spielen(void);
 
 /***********************************************************************
  * 		Variablendeklaration Global			       					   *
@@ -227,5 +228,24 @@ void kartenErstellen(void){
  **********************************************************************/
 
 void spielen(void){
+	FILE *eingabe;
+	int *p = malloc(anzahl * sizeof(char[4])); 
+	int *q = malloc(anzahl * sizeof(char[49]));
+	char code[4];
 	
+	eingabe = fopen(file, "r");
+	
+	for (i=0; i<anzahl; i++){
+		 fseek(eingabe,(long) 5+i*66, SEEK_SET);
+		 for (j=0; j<4; j++){
+			 code[j]= getc(eingabe);
+		 }
+		 printf("Code: %s\n", code);
+		 //p[i]=code;
+		// for (j=0; j<49; j++){
+			 
+		// }
+	}
+	
+	free(p);
 }
