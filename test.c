14fa38f9cc3264fb8410f4fd919a150735d3e421
Paddy <P.Szewior@live.de>
2012-08-09 11:34:02 +0200

    Spielfeldfunktionen

--- test.c
+++ test.c
@@ -1,6 +1,5 @@
 /*
  * test.c
-
  */
 
 
@@ -10,10 +9,17 @@
 #include <string.h>
 #include <ctype.h>
 
-char **spielfeld;
+char ***spielfeld;
 int n	= 5;
 int m	= 6;
-int i = 0;
+int x_size	= 0;
+int y_size  = 0;
+
+void generiereSpielfeld(int x, int y);
+void fuelleSpielfeld(char code[], int xpos, int ypos);
+void zeichneSpielfeld(void);
+void freeSpielfeld(void);
+
 
 
 int main(int argc, char **argv)
@@ -21,17 +27,21 @@ int main(int argc, char **argv)
 	generiereSpielfeld(n, m);
 	fuelleSpielfeld("2110", 0, 0);
 	fuelleSpielfeld("1030", 0, 1);
-	fuelleSpielfeld("2220", n, m);
-	zeichneSpielfeld(spielfeld);
-	free(spielfeld);
+	fuelleSpielfeld("2220", n-1, m-1);
+	zeichneSpielfeld();
+	freeSpielfeld();
 	
 	return 0;
 }
 
 void generiereSpielfeld(int x, int y)
 {
+	int i = 0;
+	int j = 0;
+	x_size	= x;
+	y_size	= y;
 	//Versuche Speicher für Spielfeld zu reservieren
-	spielfeld	= malloc((x+1)*sizeof(char[4]));
+	spielfeld	= malloc((x)*sizeof(char*));
 	if(spielfeld == NULL) 
 	{
 		//Falls kein Speicher reserviert werden konnte, gebe Meldung aus und breche das Programm sofort ab
@@ -39,14 +49,20 @@ void generiereSpielfeld(int x, int y)
 		exit(0);
 	}else
 	{
-		for(i=0; i<(x+1); i++)
+		for(i=0; i<x; i++)
 		{
-			spielfeld[i]	= malloc((y+1)*sizeof(char[4]));
+			spielfeld[i]	= malloc((y)*sizeof(char*));
 			if(spielfeld[i] == NULL)
 			{
 				//Falls kein Speicher reserviert werden konnte, gib Meldung aus und breche das Programm sofort ab
 				printf("Es konnte nicht genug Speicher für das Spielfeld reserviert werden!\n");
 				exit(0);
+			}else
+			{
+				for(j=0; j<y; j++)
+				{
+					spielfeld[i][j] = malloc(sizeof(char[4]));
+				}
 			}
 		}
 	}
@@ -57,22 +73,28 @@ void fuelleSpielfeld(char code[], int xpos, int ypos)
 	if(spielfeld == NULL){
 		printf("Fehler beim Spielfeld\n");
 	}
-	printf("%d\n", sizeof(char[4]));
-	printf("%d\n", sizeof((char*)spielfeld));
 	strcpy(spielfeld[xpos][ypos], code);
-	
-	printf("Spielfeld gefüllt! Data: %s\n", spielfeld[xpos][ypos]);
-	
 }
 
-void zeichneSpielfeld(char **feld)
+void zeichneSpielfeld(void)
 {
-	printf("Der Befehl zum Zeichnen des Spielfeldes wurde entgegen genommen\n");
-	printf("%s\n", spielfeld[0][0]);
-	printf("Der Befehl zum Zeichnen des Spielfeldes wurde entgegen genommen\n");
-	printf("%s\n", feld[0][1]);
-	printf("Der Befehl zum Zeichnen des Spielfeldes wurde entgegen genommen\n");
-	printf("%s\n", feld[n][m]);
+	/*
+	 * Hier wird dann das Spielfeld gezeichnet
+	 * */
+	 spielfeld[4][6] = "2135";
 }
 
-
+void freeSpielfeld(void)
+{
+	int k = 0;
+	int p = 0;
+	for(k=0; k<(x_size-1);k++)
+	{
+		for(p=0; p<(y_size-1); p++)
+		{
+			free(spielfeld[k][p]);
+		}
+		free(spielfeld[k]);
+	}
+	free(spielfeld);
+}
